5a5e0fae9005f4506221822702e04aaca104336d
Imane-faida <imane.faida.if@gmail.com>
2023-03-23 16:32:12 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * print_most_numbers - prints
+ * print_numbers - prints 0123456789\n
  *
  * Return: void
  */
-void print_most_numbers(void)
+
+
+void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
